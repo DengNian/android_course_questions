c78be524436c14cb91f861a0dbfaba72b46b0cc7
DengNian <dengnian602@gmail.com>
2022-06-20 21:01:29 -0400

    implement 3 Buttons(Increment,Decrement and Reset)

--- app/src/main/java/com/college/quiz1_question/MainActivity.java
+++ app/src/main/java/com/college/quiz1_question/MainActivity.java
@@ -3,6 +3,7 @@ package com.college.quiz1_question;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,5 +20,36 @@ public class MainActivity extends AppCompatActivity {
 
         //TODO
         // Your code goes here
+
+        Button btn_reset=findViewById(R.id.buttonResetId);
+        Button btn_increment=findViewById(R.id.buttonIncId);
+        Button btn_decrement=findViewById(R.id.buttonDecId);
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCounter=0;
+                counterView.setText(String.valueOf(mCounter));//update TextView
+            }
+        });
+
+        btn_increment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCounter +=1;
+                counterView.setText(String.valueOf(mCounter));
+            }
+        });
+
+        btn_decrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mCounter >0){
+                    mCounter -=1;
+                    counterView.setText(String.valueOf(mCounter));
+                }
+            }
+        });
+
+
     }
 }
